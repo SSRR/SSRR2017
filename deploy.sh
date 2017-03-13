eb476cada97a772357643f376b32accd8f637e3c
Soeren Schwertfeger <soerensch@shanghaitech.edu.cn>
2017-03-13 21:35:18 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,14 @@
 rm -rf out
 docpad generate --env static
 
+rm -rf out/vendor/twitter-bootstrap
+
 find out -type f -name "*.html" -exec sed -ie 's,"/,"/2017/,g' {} \;
+find out -type f -name "*.html" -exec sed -ie 's,url=/,url=/2017/,g' {} \;
+
+
 
 find out -name "*.htmle" -exec rm -rf {} \;
+
+cp -r out/* ../ssrr.github.com/2017/
 
